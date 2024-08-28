b986833dcd0c15ea8e89bb430c0ef3fb145fdd63
nemesea05 <149310216+nemesea05@users.noreply.github.com>
2024-08-28 13:20:37 +0800

    Add files via upload

---
+++ DATASTRUCT/src/main/java/com/mycompany/datastruct/DATASTRUCT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datastruct;
+
+/**
+ *
+ * @author Alvin
+ */
+public class DATASTRUCT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
